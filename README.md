e78cc779a68dbb13fadf4dbfa9696c86b4c8cbea
Swapnil Sahu <sahu.swapnil496@gmail.com>
2025-01-27 15:34:00 +0530

    Milestone-1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # L-square-QTify
+
+https://www.figma.com/design/tuCV26ozM1R75Ze6ShZxKs/Qtify?node-id=1-2&t=g1a0000Z2rTj28Xb-1
+
+https://qtify-nine-orcin.vercel.app/
---
+++ qtify/src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import { Box,  Typography } from "@mui/material";
+import React from "react";
+import styles from "./Button.module.css"
+
+const Button = ({text}) => {
+return(
+    
+    <button className={styles.button}>
+        {text}
+    </button>
+    
+)
+}
+
+export default Button;
+
